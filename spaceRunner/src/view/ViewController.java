d057336320eeebd19801ef78c02966e4018e2057
Donnabauer Christian <christiandonnabauer@gmail.com>
2019-05-06 17:41:07 +0200

    created menu Buttons/Background (poorly)

--- spaceRunner/src/view/ViewController.java
+++ spaceRunner/src/view/ViewController.java
@@ -5,13 +5,20 @@
  */
 package view;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
 import javafx.stage.Stage;
 import model.SpaceRunnerButton;
 
@@ -21,27 +28,76 @@ import model.SpaceRunnerButton;
  */
 public class ViewController {
     
-    private static final int WIDTH = 800;
-    private static final int HEIGHT = 600;
+    private static final int WIDTH = 1024;
+    private static final int HEIGHT =  768;
     private AnchorPane mainPane;
     private Scene mainScene;
     private Stage mainStage;
-
+    
+    private final static int MENU_BUTTONS_START_X = 100;
+    private final static int MENU_BUTTONS_START_Y = 150;
+    
+    List<SpaceRunnerButton> menuButtons;
+    
     public ViewController() {
+        menuButtons = new ArrayList<>();
         mainPane = new AnchorPane();
         mainScene = new Scene(mainPane, WIDTH, HEIGHT);
         mainStage = new Stage();
         mainStage.setScene(mainScene); 
         createButtons();
+        createBackground();
     }
     
     public Stage getMainStage(){
         return mainStage;
     }
     
-    private void createButtons(){
-        SpaceRunnerButton button = new SpaceRunnerButton("Click me!");
+    private void addMenuButton(SpaceRunnerButton button){
+        button.setLayoutX(MENU_BUTTONS_START_X);
+        button.setLayoutY(MENU_BUTTONS_START_Y + menuButtons.size()*100);
+        menuButtons.add(button);
         mainPane.getChildren().add(button);
+    }
+    
+    private void createButtons(){
+        createStartButton();
+        createScoresButton();
+        createHelpButton();
+        createCreditsButton();
+        createExitButton();
+    }
+    
+    private void createStartButton(){
+        SpaceRunnerButton startButton = new SpaceRunnerButton("PLAY");
+        addMenuButton(startButton);
+    }
+    
+    private void createScoresButton(){
+        SpaceRunnerButton scoresButton = new SpaceRunnerButton("SCORES");
+        addMenuButton(scoresButton);
+    }
+    
+    private void createHelpButton(){
+        SpaceRunnerButton helpButton = new SpaceRunnerButton("HELP");
+        addMenuButton(helpButton);
+    }
+    
+    private void createCreditsButton(){
+        SpaceRunnerButton creditsButton = new SpaceRunnerButton("CREDITS");
+        addMenuButton(creditsButton);
+    }
+    
+    private void createExitButton(){
+        SpaceRunnerButton exitButton = new SpaceRunnerButton("EXIT");
+        addMenuButton(exitButton);
+    }
+    
+    private void createBackground(){
+        Image backgroundImage = new Image("view/ressources/purple.png", 256, 256,false, true);
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
+        
+        mainPane.setBackground(new Background(background));
     }
     
 }
